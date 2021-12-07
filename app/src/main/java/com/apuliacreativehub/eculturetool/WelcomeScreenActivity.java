d076b80e41a7d51cb3d4d85c21391e0d337ac053
BrattaFrancesco <fra.bra00@gmail.com>
2021-12-07 10:14:58 +0100

    Created WelcomeScreenActivity #6

--- app/src/main/java/com/apuliacreativehub/eculturetool/WelcomeScreenActivity.java
+++ app/src/main/java/com/apuliacreativehub/eculturetool/WelcomeScreenActivity.java
@@ -1,15 +1,22 @@
 package com.apuliacreativehub.eculturetool;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class WelcomeScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome_screen);
+        //Hide ActionBar
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null){
+            actionBar.hide();
+        }
     }
 }
