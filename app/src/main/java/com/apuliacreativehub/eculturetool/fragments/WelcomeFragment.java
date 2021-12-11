c0b58e6f157e6277bc39db802cfc0c920249fb22
fabiocaiulo <info.fabiocaiulo@gmail.com>
2021-12-11 10:11:50 +0100

    Fix material design guideline

--- app/src/main/java/com/apuliacreativehub/eculturetool/fragments/WelcomeFragment.java
+++ app/src/main/java/com/apuliacreativehub/eculturetool/fragments/WelcomeFragment.java
@@ -2,11 +2,15 @@ package com.apuliacreativehub.eculturetool.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.apuliacreativehub.eculturetool.R;
 
@@ -15,6 +19,15 @@ public class WelcomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.welcome, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        Button btnLogin = view.findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(v -> {
+
+        }) ;
     }
 
 }
