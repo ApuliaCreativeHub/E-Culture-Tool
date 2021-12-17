d5fdd867e916e54e56a249a76af7efd4973b62d8
DarkFighterLuke <luca-consoli@live.it>
2021-12-17 22:21:29 +0100

    Center map according to locale when the location permission is not granted
    Signed-off-by: DarkFighterLuke <luca-consoli@live.it>

--- app/src/main/java/com/apuliacreativehub/eculturetool/fragments/MapFragment.java
+++ app/src/main/java/com/apuliacreativehub/eculturetool/fragments/MapFragment.java
@@ -19,8 +19,16 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.os.ConfigurationCompat;
 import androidx.fragment.app.Fragment;
+import androidx.loader.content.AsyncTaskLoader;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.apuliacreativehub.eculturetool.R;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationRequest;
@@ -33,14 +41,19 @@ import com.mapbox.maps.CameraOptions;
 import com.mapbox.maps.MapView;
 import com.mapbox.maps.Style;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.Locale;
 import java.util.Objects;
 import java.util.concurrent.Executor;
 
 public class MapFragment extends Fragment {
     private MapView map;
     View v;
-    private ActivityResultLauncher<String> requestPermissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+    private final ActivityResultLauncher<String> requestPermissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
         if (isGranted) {
             // TODO: Center map accordingly to user's nation (based on GPS)
             FusedLocationProviderClient fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext());
@@ -48,13 +61,34 @@ public class MapFragment extends Fragment {
             t.addOnSuccessListener(requireActivity(), new OnSuccessListener<Location>() {
                 @Override
                 public void onSuccess(Location location) {
-                    Log.d("LOCATION", String.valueOf(location));
-                    map.getMapboxMap().setCamera(new CameraOptions.Builder().center(Point.fromLngLat(location.getLongitude(), location.getLatitude())).zoom(3.0).build());
+                    if (location != null) {
+                        Log.d("LOCATION", String.valueOf(location));
+                        map.getMapboxMap().setCamera(new CameraOptions.Builder().center(Point.fromLngLat(location.getLongitude(), location.getLatitude())).zoom(3.0).build());
+                    }
                 }
             });
 
-        }else{
-            map.getMapboxMap().setCamera(new CameraOptions.Builder().center(Point.fromLngLat(16.871871, 41.117143)).zoom(3.0).build());
+        }else {
+            String nominatimApi = "https://nominatim.openstreetmap.org/search?format=json&q=";
+            Locale currentLocale = ConfigurationCompat.getLocales(getResources().getConfiguration()).get(0);
+            RequestQueue queue = Volley.newRequestQueue(requireContext());
+            StringRequest stringRequest = new StringRequest(Request.Method.GET, nominatimApi + currentLocale.getCountry(), new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    try {
+                        JSONArray jsonArray = new JSONArray(response);
+                        double lat = jsonArray.getJSONObject(0).getDouble("lat");
+                        double lon = jsonArray.getJSONObject(0).getDouble("lon");
+                        Log.d("INFERRED LOCATION", "Lat: " + lat);
+                        Log.d("INFERRED LOCATION", "Lon: " + lon);
+                        map.getMapboxMap().setCamera(new CameraOptions.Builder().center(Point.fromLngLat(lon, lat)).zoom(3.0).build());
+                    } catch (JSONException e) {
+                        map.getMapboxMap().setCamera(new CameraOptions.Builder().zoom(3.0).build());
+                        e.printStackTrace();
+                    }
+                }
+            }, null);
+            queue.add(stringRequest);
         }
     });
 
@@ -69,18 +103,8 @@ public class MapFragment extends Fragment {
         map = v.findViewById(R.id.mapview);
         map.getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS);
         //requestPermissionsIfNecessary(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION});
-        return v;
-    }
-
-    @Override
-    public void onViewCreated(View view, Bundle savedInstanceState){
-
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
         requestPermissionLauncher.launch(Manifest.permission.ACCESS_COARSE_LOCATION);
+        return v;
     }
 
     private void requestPermissionsIfNecessary(@NonNull String[] permissions) {
