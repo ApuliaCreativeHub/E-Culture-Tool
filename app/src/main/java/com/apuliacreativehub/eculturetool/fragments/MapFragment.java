7f718cc546cd7c2b2a06af9eb6068735ee1c07be
DarkFighterLuke <luca-consoli@live.it>
2021-12-17 20:11:25 +0100

    Setting map initial location based on user location

--- app/src/main/java/com/apuliacreativehub/eculturetool/fragments/MapFragment.java
+++ app/src/main/java/com/apuliacreativehub/eculturetool/fragments/MapFragment.java
@@ -1,14 +1,19 @@
 package com.apuliacreativehub.eculturetool.fragments;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,16 +22,41 @@ import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.apuliacreativehub.eculturetool.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.mapbox.geojson.Point;
 import com.mapbox.maps.CameraOptions;
 import com.mapbox.maps.MapView;
 import com.mapbox.maps.Style;
 
 import java.util.ArrayList;
+import java.util.Objects;
+import java.util.concurrent.Executor;
 
 public class MapFragment extends Fragment {
     private MapView map;
     View v;
+    private ActivityResultLauncher<String> requestPermissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+        if (isGranted) {
+            // TODO: Center map accordingly to user's nation (based on GPS)
+            FusedLocationProviderClient fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext());
+            @SuppressLint("MissingPermission") Task<Location> t = fusedLocationClient.getCurrentLocation(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY, null);
+            t.addOnSuccessListener(requireActivity(), new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    Log.d("LOCATION", String.valueOf(location));
+                    map.getMapboxMap().setCamera(new CameraOptions.Builder().center(Point.fromLngLat(location.getLongitude(), location.getLatitude())).zoom(3.0).build());
+                }
+            });
+
+        }else{
+            map.getMapboxMap().setCamera(new CameraOptions.Builder().center(Point.fromLngLat(16.871871, 41.117143)).zoom(3.0).build());
+        }
+    });
 
     public MapFragment() {
         super(R.layout.osm_map);
@@ -38,16 +68,19 @@ public class MapFragment extends Fragment {
         v = inflater.inflate(R.layout.osm_map, null);
         map = v.findViewById(R.id.mapview);
         map.getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS);
-        requestPermissionsIfNecessary(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION});
-        registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
-            if(isGranted) {
-                // TODO: Center map accordingly to user's nation (based on GPS)
-            }else{
-                map.getMapboxMap().setCamera(new CameraOptions.Builder().center(Point.fromLngLat(16.871871, 41.117143)).zoom(3.0).build());
-            }
-        });
-
+        //requestPermissionsIfNecessary(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION});
         return v;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState){
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        requestPermissionLauncher.launch(Manifest.permission.ACCESS_COARSE_LOCATION);
     }
 
     private void requestPermissionsIfNecessary(@NonNull String[] permissions) {
