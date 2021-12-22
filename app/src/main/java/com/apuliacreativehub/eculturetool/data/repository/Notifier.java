af92345d568270d92d599d57cbeccbb8cd8a3352
BrattaFrancesco <fra.bra00@gmail.com>
2021-12-22 12:10:19 +0100

    Created RepositoryNotifier and Notifier interface

--- app/src/main/java/com/apuliacreativehub/eculturetool/data/repository/Notifier.java
+++ app/src/main/java/com/apuliacreativehub/eculturetool/data/repository/Notifier.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import retrofit2.Response;
 
 public interface Notifier {
-    <T> void notifyResult(final Response<T> response, final RepositoryCallback<T> callback);
+    <T> void notifyResult(final Response<T> response);
 
-    <T> void notifyError(final IOException ioe, RepositoryCallback<T> callback);
+    <T> void notifyError(final IOException ioe);
 }
--- app/src/main/java/com/apuliacreativehub/eculturetool/data/repository/RepositoryNotifier.java
+++ app/src/main/java/com/apuliacreativehub/eculturetool/data/repository/RepositoryNotifier.java
@@ -6,14 +6,16 @@ import java.io.IOException;
 
 import retrofit2.Response;
 
-public class UserRepositoryNotifier implements Notifier{
+public class RepositoryNotifier implements Notifier{
     private final Handler resultHandler;
+    private final RepositoryCallback callback;
 
-    public UserRepositoryNotifier(Handler resultHandler) {
+    public RepositoryNotifier(Handler resultHandler, RepositoryCallback callback) {
         this.resultHandler = resultHandler;
+        this.callback = callback;
     }
 
-    public <T> void notifyResult(final Response<T> response, final RepositoryCallback<T> callback) {
+    public <T> void notifyResult(final Response<T> response) {
         resultHandler.post(new Runnable() {
             @Override
             public void run() {
@@ -22,7 +24,7 @@ public class UserRepositoryNotifier implements Notifier{
         });
     }
 
-    public <T> void notifyError(final IOException ioe, RepositoryCallback<T> callback) {
+    public <T> void notifyError(final IOException ioe) {
         resultHandler.post(new Runnable() {
             @Override
             public void run() {
