21f778c65a21bba7b9305fbe4ff2c46305f42b08
DarkFighterLuke <luca-consoli@live.it>
2021-12-23 23:27:52 +0100

    Implemented login method in UserRepository
    Signed-off-by: DarkFighterLuke <luca-consoli@live.it>

--- app/src/main/java/com/apuliacreativehub/eculturetool/data/repository/UserRepository.java
+++ app/src/main/java/com/apuliacreativehub/eculturetool/data/repository/UserRepository.java
@@ -4,6 +4,7 @@ import android.util.Log;
 
 import androidx.lifecycle.MutableLiveData;
 
+import com.apuliacreativehub.eculturetool.data.entity.Token;
 import com.apuliacreativehub.eculturetool.data.entity.User;
 import com.apuliacreativehub.eculturetool.data.network.RemoteUserDAO;
 import com.apuliacreativehub.eculturetool.data.network.UserRemoteDatabase;
@@ -44,5 +45,28 @@ public class UserRepository{
             }
         });
         return registrationResult;
+    }
+
+    public MutableLiveData<RepositoryNotification<Token>> loginUser(User user) {
+        Call<Token> call = remoteUserDAO.LoginUser(user);
+        MutableLiveData<RepositoryNotification<Token>> loginResult = new MutableLiveData<>();
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Response<Token> response = call.execute();
+                    Log.d("RETROFITRESPONSE", String.valueOf(response.code()));
+                    RepositoryNotification<Token> repositoryNotification = new RepositoryNotification<>();
+                    repositoryNotification.setData(response.body());
+                    loginResult.postValue(repositoryNotification);
+                } catch (IOException ioe) {
+                    RepositoryNotification<Token> repositoryNotification = new RepositoryNotification<>();
+                    repositoryNotification.setException(ioe);
+                    loginResult.postValue(repositoryNotification);
+                    Log.e("RETROFITERROR", ioe.getMessage());
+                }
+            }
+        });
+        return loginResult;
     }
 }
