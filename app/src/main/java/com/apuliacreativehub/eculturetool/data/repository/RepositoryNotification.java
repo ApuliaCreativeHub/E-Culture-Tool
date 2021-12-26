1a66dabfd6d38a62c5b4479a7932558b63a861cb
DarkFighterLuke <luca-consoli@live.it>
2021-12-26 23:57:18 +0100

    Added error field to RepositoryNotification

--- app/src/main/java/com/apuliacreativehub/eculturetool/data/repository/RepositoryNotification.java
+++ app/src/main/java/com/apuliacreativehub/eculturetool/data/repository/RepositoryNotification.java
@@ -2,6 +2,7 @@ package com.apuliacreativehub.eculturetool.data.repository;
 
 public class RepositoryNotification<T> implements RepositoryNotificationInterface<T> {
     private T data;
+    private String errorMessage;
     private Exception exception;
 
     public T getData() {
@@ -18,5 +19,13 @@ public class RepositoryNotification<T> implements RepositoryNotificationInterfac
 
     public void setException(Exception exception) {
         this.exception = exception;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
     }
 }
