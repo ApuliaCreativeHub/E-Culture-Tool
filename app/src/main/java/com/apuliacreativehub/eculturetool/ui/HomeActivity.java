1b3ed2811bad0001877fec0ad62a19dacde01d46
DarkFighterLuke <luca-consoli@live.it>
2022-01-17 19:05:33 +0100

    Implemented AppBar

--- app/src/main/java/com/apuliacreativehub/eculturetool/ui/HomeActivity.java
+++ app/src/main/java/com/apuliacreativehub/eculturetool/ui/HomeActivity.java
@@ -2,17 +2,24 @@ package com.apuliacreativehub.eculturetool.ui;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.apuliacreativehub.eculturetool.R;
 import com.apuliacreativehub.eculturetool.data.TokenManager;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class HomeActivity extends AppCompatActivity {
     public static final int SHOW_PATHS = 0;
@@ -36,6 +43,9 @@ public class HomeActivity extends AppCompatActivity {
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationBar);
         navController = Navigation.findNavController(this, R.id.navHostContainer);
+        AppBarConfiguration appBarConfiguration= new AppBarConfiguration.Builder(R.id.pathsFragment, R.id.placesFragment, R.id.userFragment).build();
+        Toolbar toolbar=findViewById(R.id.homeToolbar);
+        NavigationUI.setupWithNavController(toolbar, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
 
