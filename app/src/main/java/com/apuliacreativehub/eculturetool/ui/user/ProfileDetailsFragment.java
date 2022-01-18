e1213e5733ebe1ddcc85c1fa918202efad48afd6
DarkFighterLuke <luca-consoli@live.it>
2022-01-18 17:39:58 +0100

    Implemented toolbar in ProfileDetailsFragment

--- app/src/main/java/com/apuliacreativehub/eculturetool/ui/user/ProfileDetailsFragment.java
+++ app/src/main/java/com/apuliacreativehub/eculturetool/ui/user/ProfileDetailsFragment.java
@@ -14,9 +14,15 @@ import android.widget.TextView;
 import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import com.apuliacreativehub.eculturetool.R;
 
@@ -54,15 +60,20 @@ public class ProfileDetailsFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        Toolbar toolbar=view.findViewById(R.id.profileToolbar);
+        toolbar.setTitle(R.string.profile_details_screen_title);
+        AppCompatActivity activity=(AppCompatActivity) requireActivity();
+        activity.setSupportActionBar(toolbar);
+        ActionBar actionBar= activity.getSupportActionBar();
+        assert actionBar != null;
+        actionBar.setDisplayHomeAsUpEnabled(false);
+
         logoutViewModel = new ViewModelProvider(this).get(LogoutViewModel.class);
 
-        Context context = getActivity();
-        if (context != null) {
-            SharedPreferences sharedPref = context.getSharedPreferences(getString(R.string.login_shared_preferences), Context.MODE_PRIVATE);
-            ((TextView)view.findViewById(R.id.txtName)).setText(sharedPref.getString("name", null));
-            ((TextView)view.findViewById(R.id.txtSurname)).setText(sharedPref.getString("surname", null));
-            ((TextView)view.findViewById(R.id.txtEmail)).setText(sharedPref.getString("email", null));
-        }
+        SharedPreferences sharedPref = activity.getSharedPreferences(getString(R.string.login_shared_preferences), Context.MODE_PRIVATE);
+        ((TextView)view.findViewById(R.id.txtName)).setText(sharedPref.getString("name", null));
+        ((TextView)view.findViewById(R.id.txtSurname)).setText(sharedPref.getString("surname", null));
+        ((TextView)view.findViewById(R.id.txtEmail)).setText(sharedPref.getString("email", null));
     }
 
     public void onStart() {
