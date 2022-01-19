1033cfc5cd245e30d5e231ce6379951524a4bd34
fabiocaiulo <info.fabiocaiulo@gmail.com>
2022-01-19 21:51:06 +0100

    Pick image from gallery

--- app/src/main/java/com/apuliacreativehub/eculturetool/ui/places/CreatePlaceFragment.java
+++ app/src/main/java/com/apuliacreativehub/eculturetool/ui/places/CreatePlaceFragment.java
@@ -1,5 +1,8 @@
 package com.apuliacreativehub.eculturetool.ui.places;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -9,13 +12,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.apuliacreativehub.eculturetool.R;
 
+@SuppressWarnings("deprecation")
 public class CreatePlaceFragment extends Fragment {
+    private View view;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -23,8 +31,30 @@ public class CreatePlaceFragment extends Fragment {
     }
 
     @Override
+    public void onStart() {
+        super.onStart();
+
+        TextView btnPickImage = view.findViewById(R.id.btnPickImage);
+        btnPickImage.setOnClickListener(view -> {
+            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(intent, 3);
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == Activity.RESULT_OK && data != null) {
+            Uri selectedImage = data.getData();
+            ImageView imgPlace = view.findViewById(R.id.imgPlace);
+            imgPlace.setImageURI(selectedImage);
+        }
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_create_place, container, false);
+        view = inflater.inflate(R.layout.fragment_create_place, container, false);
+        return view;
     }
 
     @Override
