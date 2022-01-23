17d814a630aef43c07ed760fd245ee479d35f300
lcorallo <l.corallo2@gmail.com>
2022-01-23 12:59:22 +0100

    Moving Manage Artifacts into correct screen

--- app/src/main/java/com/apuliacreativehub/eculturetool/ui/places/ManagePlaceFragment.java
+++ app/src/main/java/com/apuliacreativehub/eculturetool/ui/places/ManagePlaceFragment.java
@@ -1,19 +1,51 @@
 package com.apuliacreativehub.eculturetool.ui.places;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.apuliacreativehub.eculturetool.R;
+import com.apuliacreativehub.eculturetool.ui.component.ListArtifactsAdapter;
 import com.apuliacreativehub.eculturetool.ui.component.TransactionHelper;
 
+import java.util.ArrayList;
+
 public class ManagePlaceFragment extends Fragment {
+
+    private static final int NUMBER_COLUMN = 2;
+
+    private View view;
+    private ArrayAdapter arrayOptionsAdapter;
+    private AutoCompleteTextView autoCompleteTextView;
+    private RecyclerView recyclerGridView;
+    private GridLayoutManager gridLayoutManager;
+    private ListArtifactsAdapter listArtifactsAdapter;
+    private ArrayList<String> mDataset;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mDataset = new ArrayList<>();
+        mDataset.add("CIAO");
+        mDataset.add("CIAO1");
+        mDataset.add("CIAO2");
+        mDataset.add("CIAO3");
+        mDataset.add("CIAO4");
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -23,7 +55,9 @@ public class ManagePlaceFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
+        this.view = view;
+        setSelectElement();
+        setDynamicRecycleView();
         Toolbar toolbar = view.findViewById(R.id.managePlaceToolbar);
         toolbar.setTitle(R.string.manage_place_screen_title);
         toolbar.inflateMenu(R.menu.top_menu_manage_place);
@@ -38,4 +72,33 @@ public class ManagePlaceFragment extends Fragment {
         toolbar.setNavigationOnClickListener(v -> requireActivity().getSupportFragmentManager().popBackStack());
     }
 
+    private void setSelectElement() {
+        //TODO: Fetch room
+        arrayOptionsAdapter = new ArrayAdapter(getContext(), R.layout.item_select_room, new String[]{"Stanza A", "Stanza B", " Stanza C"});
+        autoCompleteTextView = view.findViewById(R.id.selectRoomAutoComplete);
+        autoCompleteTextView.setAdapter(arrayOptionsAdapter);
+
+        autoCompleteTextView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                Log.i("ENTER", "OK");
+                return false;
+            }
+        });
+
+        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.i("CLICK", "OK");
+            }
+        });
+    }
+
+    private void setDynamicRecycleView() {
+        recyclerGridView = view.findViewById(R.id.recyclerContainerObject);
+        gridLayoutManager = new GridLayoutManager(getContext(), NUMBER_COLUMN);
+        recyclerGridView.setLayoutManager(gridLayoutManager);
+        listArtifactsAdapter = new ListArtifactsAdapter(mDataset);
+        recyclerGridView.setAdapter(listArtifactsAdapter);
+    }
 }
