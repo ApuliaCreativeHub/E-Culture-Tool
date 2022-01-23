40d97e3ba89dac74a945531678227d05cca509ee
DarkFighterLuke <luca-consoli@live.it>
2022-01-23 23:38:09 +0100

    Fixed API endpoint for adding places
    Signed-off-by: DarkFighterLuke <luca-consoli@live.it>

--- app/src/main/java/com/apuliacreativehub/eculturetool/data/network/place/RemotePlaceDAO.java
+++ app/src/main/java/com/apuliacreativehub/eculturetool/data/network/place/RemotePlaceDAO.java
@@ -1,19 +1,14 @@
 package com.apuliacreativehub.eculturetool.data.network.place;
 
-import com.apuliacreativehub.eculturetool.data.entity.Place;
-
 import okhttp3.RequestBody;
 import retrofit2.Call;
-import retrofit2.http.Body;
-import retrofit2.http.Headers;
 import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.Part;
 
 public interface RemotePlaceDAO {
     @Multipart
-    @Headers("Content-Type: multipart/form-data")
-    @POST("user/add")
+    @POST("place/add")
     Call<Void> AddPlace( @Part("name")RequestBody name,
                         @Part("address")RequestBody address,
                         @Part("description")RequestBody description,
