527a254fc8916e6f4038cebb89034f128a3c438e
BrattaFrancesco <fra.bra00@gmail.com>
2022-01-23 11:19:57 +0100

    Add place implementation (resolve bug to pick external res)

--- app/src/main/java/com/apuliacreativehub/eculturetool/ui/places/CreatePlaceViewModel.java
+++ app/src/main/java/com/apuliacreativehub/eculturetool/ui/places/CreatePlaceViewModel.java
@@ -2,10 +2,20 @@ package com.apuliacreativehub.eculturetool.ui.places;
 
 import android.app.Application;
 
+import androidx.lifecycle.MutableLiveData;
+
+import com.apuliacreativehub.eculturetool.data.entity.Place;
+import com.apuliacreativehub.eculturetool.data.entity.user.User;
+import com.apuliacreativehub.eculturetool.data.repository.RepositoryNotification;
+
 public class CreatePlaceViewModel extends AbstractPlaceViewModel {
 
     public CreatePlaceViewModel(Application application) {
         super(application);
     }
 
+    public MutableLiveData<RepositoryNotification<Void>> addPlace(){
+        Place place = new Place(name, address, description, image.getPath());
+        return repository.addPlace(place);
+    }
 }
