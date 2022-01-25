22617211f0f5021ed4d5536014c937e9d2e3f42b
DarkFighterLuke <luca-consoli@live.it>
2022-01-25 21:54:12 +0100

    Created zone entity
    Signed-off-by: DarkFighterLuke <luca-consoli@live.it>

---
+++ app/src/main/java/com/apuliacreativehub/eculturetool/data/entity/Zone.java
@@ -0,0 +1,38 @@
+package com.apuliacreativehub.eculturetool.data.entity;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class Zone {
+    @PrimaryKey
+    private int id;
+
+    @ColumnInfo(name = "name")
+    private String name;
+
+    public Zone() {
+    }
+
+    public Zone(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
