cc7abca5697ff14ea4eafe71fcb41fe04b016e62
DarkFighterLuke <luca-consoli@live.it>
2022-01-25 23:30:50 +0100

    Started implementation of ManagePlaceViewModel
    Signed-off-by: DarkFighterLuke <luca-consoli@live.it>

---
+++ app/src/main/java/com/apuliacreativehub/eculturetool/ui/places/ManagePlaceViewModel.java
@@ -0,0 +1,53 @@
+package com.apuliacreativehub.eculturetool.ui.places;
+
+import android.app.Application;
+import android.content.Context;
+import android.net.ConnectivityManager;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.MutableLiveData;
+
+import com.apuliacreativehub.eculturetool.data.entity.Place;
+import com.apuliacreativehub.eculturetool.data.entity.Zone;
+import com.apuliacreativehub.eculturetool.data.repository.RepositoryNotification;
+import com.apuliacreativehub.eculturetool.data.repository.ZoneRepository;
+import com.apuliacreativehub.eculturetool.di.ECultureTool;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ManagePlaceViewModel extends AndroidViewModel {
+    private final ZoneRepository zoneRepository;
+    private List<Zone> zones;
+    private Place place;
+
+    protected ManagePlaceViewModel(Application application) {
+        super(application);
+        ECultureTool app = getApplication();
+        zoneRepository = new ZoneRepository(app.executorService, app.localDatabase, (ConnectivityManager) app.getSystemService(Context.CONNECTIVITY_SERVICE));
+    }
+
+    public Place getPlace() {
+        return place;
+    }
+
+    public void setPlace(Place place) {
+        this.place = place;
+    }
+
+    public MutableLiveData<RepositoryNotification<ArrayList<Zone>>> getZonesFromDatabase() {
+        if (place != null) {
+            return zoneRepository.getAllPlaceZones(place);
+        } else {
+            return new MutableLiveData<>();
+        }
+    }
+
+    public List<Zone> getZones() {
+        return zones;
+    }
+
+    public void setZones(List<Zone> zones) {
+        this.zones = zones;
+    }
+}
