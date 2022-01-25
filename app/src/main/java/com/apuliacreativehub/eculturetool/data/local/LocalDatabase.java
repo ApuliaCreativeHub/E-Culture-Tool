20ef37190f085f067f3caf370602176a3144e2a4
DarkFighterLuke <luca-consoli@live.it>
2022-01-25 14:44:17 +0100

    Added exportSchema parameter to remove compile time warnings
    Signed-off-by: DarkFighterLuke <luca-consoli@live.it>

--- app/src/main/java/com/apuliacreativehub/eculturetool/data/local/LocalDatabase.java
+++ app/src/main/java/com/apuliacreativehub/eculturetool/data/local/LocalDatabase.java
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase;
 
 import com.apuliacreativehub.eculturetool.data.entity.Place;
 
-@Database(entities = {Place.class}, version = 1)
+@Database(entities = {Place.class}, version = 1, exportSchema = false)
 public abstract class LocalDatabase extends RoomDatabase {
     public abstract LocalPlaceDAO placeDAO();
 
