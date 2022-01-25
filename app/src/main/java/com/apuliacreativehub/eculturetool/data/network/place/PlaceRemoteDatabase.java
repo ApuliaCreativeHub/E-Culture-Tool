e8f361607e94ae2ba156b36dcfeb0cb7c4e67922
BrattaFrancesco <fra.bra00@gmail.com>
2022-01-25 18:04:04 +0100

    Changed webServer ip

--- app/src/main/java/com/apuliacreativehub/eculturetool/data/network/place/PlaceRemoteDatabase.java
+++ app/src/main/java/com/apuliacreativehub/eculturetool/data/network/place/PlaceRemoteDatabase.java
@@ -9,8 +9,8 @@ public class PlaceRemoteDatabase extends RemoteDatabase {
     public static RemotePlaceDAO provideRemotePlaceDAO() {
         Retrofit retrofit = new Retrofit.Builder()
                 .client(client)
-                //.baseUrl("https://hiddenfile.ml/ecultureapi/")
-                .baseUrl("http://10.0.2.2:8080/")
+                .baseUrl("https://hiddenfile.ml/ecultureapi/")
+                //.baseUrl("http://10.0.2.2:8080/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
