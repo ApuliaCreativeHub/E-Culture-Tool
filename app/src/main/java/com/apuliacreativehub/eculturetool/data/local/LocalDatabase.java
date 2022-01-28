3480f2d1eb32e4587209083e654dd4de2d165192
DarkFighterLuke <luca-consoli@live.it>
2022-01-28 23:09:29 +0100

    Added Object entity to local database schema
    Signed-off-by: DarkFighterLuke <luca-consoli@live.it>

--- app/src/main/java/com/apuliacreativehub/eculturetool/data/local/LocalDatabase.java
+++ app/src/main/java/com/apuliacreativehub/eculturetool/data/local/LocalDatabase.java
@@ -3,10 +3,11 @@ package com.apuliacreativehub.eculturetool.data.local;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
+import com.apuliacreativehub.eculturetool.data.entity.Object;
 import com.apuliacreativehub.eculturetool.data.entity.Place;
 import com.apuliacreativehub.eculturetool.data.entity.Zone;
 
-@Database(entities = {Place.class, Zone.class}, version = 1, exportSchema = false)
+@Database(entities = {Place.class, Zone.class, Object.class}, version = 1, exportSchema = false)
 public abstract class LocalDatabase extends RoomDatabase {
     public abstract LocalPlaceDAO placeDAO();
 
