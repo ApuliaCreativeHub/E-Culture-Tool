fcb69ce77ef62fd6cb9bcfa651f639ae3c762627
DarkFighterLuke <luca-consoli@live.it>
2022-01-30 23:15:34 +0100

    Added method in LocalDatabase to provide LocalPathDAO
    Signed-off-by: DarkFighterLuke <luca-consoli@live.it>

--- app/src/main/java/com/apuliacreativehub/eculturetool/data/local/LocalDatabase.java
+++ app/src/main/java/com/apuliacreativehub/eculturetool/data/local/LocalDatabase.java
@@ -3,17 +3,21 @@ package com.apuliacreativehub.eculturetool.data.local;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
+import com.apuliacreativehub.eculturetool.data.entity.IsPresentIn;
 import com.apuliacreativehub.eculturetool.data.entity.Object;
+import com.apuliacreativehub.eculturetool.data.entity.Path;
 import com.apuliacreativehub.eculturetool.data.entity.Place;
 import com.apuliacreativehub.eculturetool.data.entity.Zone;
 
-@Database(entities = {Place.class, Zone.class, Object.class}, version = 1, exportSchema = false)
+@Database(entities = {Place.class, Zone.class, Object.class, IsPresentIn.class, Path.class}, version = 1, exportSchema = false)
 public abstract class LocalDatabase extends RoomDatabase {
     public abstract LocalPlaceDAO placeDAO();
 
     public abstract LocalZoneDAO zoneDAO();
 
     public abstract LocalObjectDAO objectDAO();
+
+    public abstract LocalPathDAO pathDAO();
 
     // Add other DAOs abstract methods here
 }
