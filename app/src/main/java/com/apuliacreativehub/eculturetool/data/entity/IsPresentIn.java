10d16a9436b0ddecff4e2c90e7b2fc2527b4bd7a
DarkFighterLuke <luca-consoli@live.it>
2022-01-30 23:13:44 +0100

    Implemented IsPresentIn entity with basic fields
    Signed-off-by: DarkFighterLuke <luca-consoli@live.it>

---
+++ app/src/main/java/com/apuliacreativehub/eculturetool/data/entity/IsPresentIn.java
@@ -0,0 +1,10 @@
+package com.apuliacreativehub.eculturetool.data.entity;
+
+import androidx.room.Entity;
+
+@Entity
+public class IsPresentIn {
+    int objectId;
+    int pathId;
+    int order;
+}
