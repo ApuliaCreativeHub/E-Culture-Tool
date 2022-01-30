7d7b32024d59e993d91b93c060b1138427c2a3fc
DarkFighterLuke <luca-consoli@live.it>
2022-01-30 14:08:38 +0100

    Implemented RemotePathDAO
    Signed-off-by: DarkFighterLuke <luca-consoli@live.it>

---
+++ app/src/main/java/com/apuliacreativehub/eculturetool/data/network/path/RemotePathDAO.java
@@ -0,0 +1,28 @@
+package com.apuliacreativehub.eculturetool.data.network.path;
+
+import com.apuliacreativehub.eculturetool.data.entity.Path;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+
+public interface RemotePathDAO {
+    @POST("path/add")
+    Call<Path> addPath(@Body Path path);
+
+    @GET("path/getYourPaths")
+    Call<List<Path>> getYourPaths(@Query("userId") int userId);
+
+    @GET("path/getPlacePaths")
+    Call<List<Path>> getPlacePaths(@Query("placeId") int placeId);
+
+    @POST("path/update")
+    Call<Void> editPath(@Body Path path);
+
+    @POST("path/delete")
+    Call<Void> deletePath(@Body Path path);
+}
