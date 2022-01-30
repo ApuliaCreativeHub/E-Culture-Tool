417ddc75871385d009e8ee55532ef09e389f71d9
DarkFighterLuke <luca-consoli@live.it>
2022-01-30 23:14:39 +0100

    Implemented LocalPathDAO
    Signed-off-by: DarkFighterLuke <luca-consoli@live.it>

---
+++ app/src/main/java/com/apuliacreativehub/eculturetool/data/local/LocalPathDAO.java
@@ -0,0 +1,30 @@
+package com.apuliacreativehub.eculturetool.data.local;
+
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.apuliacreativehub.eculturetool.data.entity.Path;
+
+public interface LocalPathDAO {
+    @Insert
+    void insertPath(Path path);
+
+    @Query("SELECT * FROM path WHERE user_id=:userId")
+    void getAllPathsByUserId(int userId);
+
+    @Query("SELECT * FROM path " +
+            "JOIN isPresentIn ON path.id=isPresentIn.pathId " +
+            "JOIN object ON isPresentIn.objectId=object.id " +
+            "JOIN zone ON object.zone_id=zone.id " +
+            "JOIN place ON zone.place_id=place.id " +
+            "WHERE place.id=:placeId")
+    void getAllPathsByPlaceId(int placeId);
+
+    @Update
+    void updatePath(Path path);
+
+    @Delete
+    void deletePath(Path path);
+}
