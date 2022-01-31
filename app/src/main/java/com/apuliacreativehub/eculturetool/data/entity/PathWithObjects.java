0fd73e28bf51b3356424153632eea8fc8956aad8
DarkFighterLuke <luca-consoli@live.it>
2022-01-31 15:58:29 +0100

    Implemented PathWithObjects to contain results of path related queries
    Signed-off-by: DarkFighterLuke <luca-consoli@live.it>

---
+++ app/src/main/java/com/apuliacreativehub/eculturetool/data/entity/PathWithObjects.java
@@ -0,0 +1,19 @@
+package com.apuliacreativehub.eculturetool.data.entity;
+
+import androidx.room.Embedded;
+import androidx.room.Junction;
+import androidx.room.Relation;
+
+import java.util.List;
+
+public class PathWithObjects {
+    @Embedded
+    public Path path;
+
+    @Relation(
+            parentColumn = "path_id",
+            entityColumn = "object_id",
+            associateBy = @Junction(IsPresentIn.class)
+    )
+    public List<java.lang.Object> objects;
+}
