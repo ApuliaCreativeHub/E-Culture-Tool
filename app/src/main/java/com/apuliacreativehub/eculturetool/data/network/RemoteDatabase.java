47f1c401a225502ef8b4b8b7bf157c22fa277c8a
DarkFighterLuke <luca-consoli@live.it>
2022-02-14 15:57:17 +0100

    Made RemoteDatabase concrete
    Signed-off-by: DarkFighterLuke <luca-consoli@live.it>

--- app/src/main/java/com/apuliacreativehub/eculturetool/data/network/RemoteDatabase.java
+++ app/src/main/java/com/apuliacreativehub/eculturetool/data/network/RemoteDatabase.java
@@ -14,7 +14,7 @@ import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public abstract class RemoteDatabase {
+public class RemoteDatabase {
     private static final String baseUrl = BuildConfig.API_URL;
 
     protected static OkHttpClient client = new OkHttpClient.Builder().addInterceptor(new Interceptor() {
